5e8a85cd77c468f54259c124fce3fae10faf3688
Pranjal Soni <30702374+Pranjal-Soni@users.noreply.github.com>
2019-06-20 18:23:04 +0530

    Add files via upload

---
+++ webscrapper.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 20 17:49:09 2019
+
+@author: pranjal
+"""
+
+import os,bs4,requests
+
+url = 'http://xkcd.com'
+os.makedirs('Web Scrapping',exist_ok = True)
+
+while not url.endswith('#'):
+                       print('Downloading the page %s...'%url)
+                       res = requests.get(url)
+                       res.raise_for_status()
+                       
+                       soup = bs4.BeautifulSoup(res.text)
+                       comicElem = soup.select('#comic img')
+                       if comicElem == []:
+                           print('Could not find comic image.')
+                       else:
+                           comicUrl = comicElem[0].get('src')
+                           print('Downloading image %s...'%(comicUrl))
+                           res = requests.get(comicUrl)
+                           res.raise_for_status()
+                           imageFile = open(os.path.join('Web Scrapping',os.path.basename(comicUrl)),'wb')
+                           for chunk in res.iter_content(100000):
+                               imageFile.write(chunk)
+                           imageFile.close()
+                       prevLink = soup.select('a[rel= "prev"]')[0]
+                       url = 'http://xkcd.com' + prevLink.get('href')
+print('Done.')
